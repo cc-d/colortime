1bc3774e743d927c3a527079b6132ab78240ce82
Cary Carter <ccarterdev@gmail.com>
2025-01-07 22:06:59 +0000

    1 FILE main.py

--- main.py
+++ main.py
@@ -102,40 +102,46 @@ VAL_COLORS: List[str] = [
     if fg[1] != bg[1] and bg[1] 
 ]
 
+FG_LEN = len(FG_COLORS)
+BG_LEN = len(BG_COLORS)
 
-def get_color(seconds: int) -> str:
-    idx, ccount = 0, len(VAL_COLORS)
-    while seconds > 0:
-        if seconds - ccount * REPEAT > 0:
-            seconds -= ccount * REPEAT
-            idx = 0
-        else:
-            seconds -= REPEAT
-            idx += 1
-            if idx >= len(VAL_COLORS):
-                idx = 0
-    return VAL_COLORS[idx]
+cur_cols = (0, 0)
+
+def get_color(repeat_inc: int) -> str:
+    if repeat_inc >= FG_LEN:
+        cur_cols[0] += 1
+        if cur_cols[0] >= FG_LEN:
+            cur_cols[0] = 0
+            cur_cols[1] += 1
+            if cur_cols[1] >= BG_LEN:
+                cur_cols[1] = 0
+    return ''.join(str(c) for c in cur_cols)
 
 
-def write_time(wf: Callable[[str], None], msg: str) -> str:
+def write_time(wf: Callable[[str], None], msg: str, rinc: int) -> str:
     hr, mn, sec = map(int, msg.split(":"))
-    total_seconds = (60 * 60 * hr) + (60 * mn) + sec
-    color = get_color(total_seconds)
+
+
+    hr = f'{hr}'[1:]
+    print('hr', hr, f'{hr}'[1:])
+    color = get_color(rinc)
     formatted_time = (
-        f"{color}{hr if hr < 10 else str(hr)[1:]}:{mn:02}:{sec:02}\033[0m\n"
+        f"{color}{hr}:{mn:02}:{sec:02}"
     )
-    wf(formatted_time)
+    wf(formatted_time + "\033[0m\n")
     return formatted_time
 
 
 def main() -> None:
     wf: Callable[[str], None] = stdout.write
     last_time: str | None = None
+    rinc = 0
     while True:
         current_time = dt.now(UTC).strftime("%H:%M:%S")
         if current_time != last_time:
-            write_time(wf, current_time)
+            write_time(wf, current_time, rinc)
             last_time = current_time
+            rinc += 1
         sleep(DELAY)
 
 
